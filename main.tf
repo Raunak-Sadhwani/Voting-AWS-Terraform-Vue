983814638d13901ab685f2da30a6cc5d8831d2db
raunak <raunak200308@gmail.com>
2024-06-21 13:38:50 +0200

    polx

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ resource "aws_s3_bucket_policy" "firstbucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        "Sid"     = "Statement1"
-        "Effect"   = "Allow" 
-        "Principal" = "*"
-        "Action"    = "s3:GetObject"
-        "Resource" = "arn:aws:s3:::group10first/*"
+        Sid       = "Statement1"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::group10first/*"
       }
     ]
   })
