77cdec14d4bbf16dd98e097c37e5566144caa3a1
“agawade” <agawade@stud.hs-heilbronn.de>
2024-06-21 18:49:10 +0200

    try

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
-terraform {
+/* terraform {
     backend "http" {
         address = "https://gitlab.com/api/v4/projects/6309/terraform/state/tf_state"
-        username = "rsadhwani"
-        password = "glpat-s3BzZCbszcC6oGVBsRmU"
     }
 }
+ */
