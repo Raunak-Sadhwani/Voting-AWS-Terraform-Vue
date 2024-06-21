7bfcfb5c61c220381faec960be9fa43a6452aa95
“agawade” <agawade@stud.hs-heilbronn.de>
2024-06-21 13:49:37 +0200

    Commit after changes in policy

--- main.tf
+++ main.tf
@@ -112,20 +112,3 @@ output "bucket_url" {
 }
 
 
-
-resource "aws_s3_bucket_policy" "firstbucket_policy" {
-  bucket = aws_s3_bucket.firstbucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "Statement1"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::group10first/*"
-      }
-    ]
-  })
-}
