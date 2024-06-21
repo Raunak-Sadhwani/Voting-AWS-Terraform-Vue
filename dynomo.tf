1ac1601b78eaaf933408cc2489c4e0b742b2c973
raunak <raunak200308@gmail.com>
2024-06-21 17:25:56 +0200

    test12 adi

--- dynomo.tf
+++ dynomo.tf
@@ -1,5 +1,3 @@
-
-
 # resource "aws_dynamodb_table" "companies" {
 #   name           = "companies"
 #   billing_mode   = "PAY_PER_REQUEST"
--- output.tf
+++
@@ -1,3 +0,0 @@
-output "api-gateway-url" {
-  value = aws_api_gateway_deployment.example.invoke_url
-}
