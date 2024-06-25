a02f6577fbcea6c966f17413272030bca4137904
raunak <raunak200308@gmail.com>
2024-06-25 19:42:02 +0200

    removed axios

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import { createApp } from "vue";
 
 import router from "./router.js";
 import store from "./store.js";
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 import App from "./App.vue";
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
-app.use(VueAxios, axios)
 app.mount("#app");
